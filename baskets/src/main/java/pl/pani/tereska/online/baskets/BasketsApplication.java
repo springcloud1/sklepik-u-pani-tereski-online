e086b92a3174fb46a44ecbc672d914a7a4e03cce
qwerty12342019 <malpa123!>
2019-10-22 11:46:44 +0200

    add baskets

---
+++ baskets/src/main/java/pl/pani/tereska/online/baskets/BasketsApplication.java
@@ -0,0 +1,13 @@
+package pl.pani.tereska.online.baskets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasketsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasketsApplication.class, args);
+    }
+
+}
---
+++ baskets/src/test/java/pl/pani/tereska/online/baskets/BasketsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pani.tereska.online.baskets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasketsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
