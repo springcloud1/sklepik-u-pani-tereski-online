266973e3a8f5f70d68b1f24f35ca66964a4c4746
springcloud1 <zaq1@WSXcvb>
2019-10-22 11:41:49 +0200

    add configuration, discovery

--- configuration/src/main/java/pl/pani/tereska/online/configuration/ConfigurationApplication.java
+++ configuration/src/main/java/pl/pani/tereska/online/configuration/ConfigurationApplication.java
@@ -2,7 +2,9 @@ package pl.pani.tereska.online.configuration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigurationApplication {
 
